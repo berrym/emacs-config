72f38ff4f7e0287b5a6e77a1b866bc2bca71bfa0
Michael Berry <trismegustis@gmail.com>
2015-10-09 05:47:28 -0400

    Remember to load custom-file.

--- init.el
+++ init.el
@@ -18,6 +18,7 @@
 (setq tramp-auto-save-directory (file-name-as-directory save-files-dir))
 (setq custom-file (expand-file-name "custom.el" (file-name-as-directory
 						 save-files-dir)))
+(load custom-file)
 
 (add-to-list 'load-path modules-dir)
 (require 'berrym-packages)
